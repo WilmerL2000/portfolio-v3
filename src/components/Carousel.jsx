b0f379fe5b6f5bebe4e15149916b140a537b77e5
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2024-05-17 17:33:30 -0600

    Little modification

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -9,13 +9,19 @@ import {
 
 export function CarouselDemo({ images }) {
   return (
-    <Carousel className="w-full max-w-3xl container ">
+    <Carousel className="w-full max-w-3xl container">
       <CarouselContent>
         {images.map(({ url, _key, _type }) => (
           <CarouselItem key={_key}>
             <Card>
               <CardContent className="flex aspect-video items-center justify-center">
-                <img src={url} alt={`Project image ${_type}`} loading="lazy" />
+                <picture>
+                  <img
+                    src={url}
+                    alt={`Project image ${_type}`}
+                    loading="lazy"
+                  />
+                </picture>
               </CardContent>
             </Card>
           </CarouselItem>
